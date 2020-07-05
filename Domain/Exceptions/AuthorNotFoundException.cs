b8bb5808ef6a6dc3f8391d965cf9fac29e70031e
karsujo <karsujo@gmail.com>
2020-07-05 20:09:37 +0530

    Custom AuthorNotFound Exception

---
+++ Domain/Exceptions/AuthorNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Exceptions
+{
+    public class AuthorNotFoundException : Exception
+    {
+        public AuthorNotFoundException(string authorId, Exception ex) : base($"Author for Id : {authorId} not found", ex) { }
+    
+    }
+}
--- Domain/Exceptions/BookNotFoundException.cs
+++ Domain/Exceptions/BookNotFoundException.cs
@@ -6,6 +6,6 @@ namespace OdysseyPublishers.Domain.Exceptions
 {
     public class BookNotFoundException :Exception
     {
-        public BookNotFoundException(string book, Exception ex) : base($"Book : {book} not found ", ex) { }
+        public BookNotFoundException(string book, Exception ex) : base($"Book with Id: {book} not found ", ex) { }
     }
 }
