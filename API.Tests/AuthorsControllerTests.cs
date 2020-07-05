bbeb54c3c3f6fc181eeedd99396a5e747167cfa1
karsujo <karsujo@gmail.com>
2020-07-05 19:07:35 +0530

    Full flow - Get Authors

--- API.Tests/AuthorsControllerTests.cs
+++ API.Tests/AuthorsControllerTests.cs
@@ -1,19 +1,46 @@
-using Moq;
-using OdysseyPublishers.API.Controllers;
+using Application.Authors;
+using Application.Books;
+using Application.Common;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using OdysseyPublishers.Application.Authors;
-using OdysseyPublishers.Application.Common;
+using OdysseyPublishers.Domain;
 using OdysseyPublishers.Infrastructure.Common;
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using Xunit;
 
 namespace API.Tests
 {
     public class AuthorsControllerTests
     {
-        [Fact]
-        public void Test1()
+        private readonly IAuthorsRepository authorsRepository;
+        public AuthorsControllerTests()
         {
-         
+            var config = new MapperConfiguration(opt =>
+            {
+                opt.AddProfile(new AuthorDbProfile());
+                opt.AddProfile(new BookDbProfile());
+            });
+
+            var mapper = config.CreateMapper();
+
+            var test = mapper.Map<Author>(new AuthorDbEntity());
+
+            var opt = Options.Create(new PersistenceConfigurations());
+
+            opt.Value.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=pubs;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            authorsRepository = new AuthorsRepository(new SqlRepositoryBase(opt), mapper);
+        }
+
+        [Fact]
+        public void GetAuthorsTest()
+        {
+           var res =  authorsRepository.GetAuthors();
+
+            Assert.IsType<List<Author>>(res);
 
         }
     }
--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,20 +1,30 @@
-﻿using Application.Books;
+﻿using Application.Authors;
+using Application.Books;
+using AutoMapper;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OdysseyPublishers.Application.Authors;
-using OdysseyPublishers.Domain;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Application
 {
-   public static class DependencyInjection
+    public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
         {
+            var config = new MapperConfiguration(opt =>
+            {
+                opt.AddProfile(new AuthorDbProfile());
+                opt.AddProfile(new BookDbProfile());
+            });
+
+            var mapper = config.CreateMapper();
+
+            services.AddSingleton(mapper);
             services.AddTransient<IAuthorsRepository, AuthorsRepository>();
             services.AddTransient<IBookRepository, BookRepository>();
+
+           
+
             return services;
         }
     }
