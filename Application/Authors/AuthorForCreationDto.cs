ab6b5df470ecdee08cb4139120723b5d176d8e57
karsujo <karsujo@gmail.com>
2020-08-09 12:49:17 +0530

    Added Author Validation

--- Application/Authors/AuthorForCreationDto.cs
+++ Application/Authors/AuthorForCreationDto.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Books;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Application.Authors
 {
@@ -9,13 +10,21 @@ namespace Application.Authors
         {
             Books = new HashSet<BookForCreationDto>();
         }
+        [Required]
+        [MaxLength(40)]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(40)]
         public string LastName { get; set; }
+   
         public string City { get; set; }
         public string Address { get; set; }
+        [Required]
         public string Phone { get; set; }
+ 
         public string State { get; set; }
 
+        [RegularExpression("^[0-9]{5}$")]
         public string Zip { get; set; }
 
         public ICollection<BookForCreationDto> Books { get; set; }
